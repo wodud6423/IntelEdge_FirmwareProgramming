49f774324cce7d133e19a42d1647d9fc71c129ff
wodud6423 <127078552+wodud6423@users.noreply.github.com>
2024-04-12 19:37:13 +0900

    polling.c 업데이트

--- 0412/0412_17/Src/polling.c
+++ 0412/0412_17/Src/polling.c
@@ -15,9 +15,25 @@ static void adc_callback_2(void *arg);
 // 스레드 구조를 정의하는 스레드 구조체
 typedef struct {
 	uint32_t period, count; // 폴링방식을 위한 주기와 카운터
-	bool flag;
-	void (*cbf)(void *);
+	bool flag; // 단일 코어에서 동시 작업으로 인한 에러를 방지하기위한...bool형 변수!
+	void (*cbf)(void *); // 콜백 함수(함수인자로 반드시 포인터형식(위치형식)을 받아야한다!!
 } THR_T; // thread type
+
+// 여기서 포인터에 대한 개념을 다시 익혀두고 가자..
+// 변수 포인터-> 그 변수의 시작주소
+// EX: int a[3] = {1,2,3}; &a : a가 저장된 주소값!
+// *a: 1(시작주소의 int형값)(*a=a[0])
+// &a: 1의 시작주소값
+// (우리는 STM32이니까 32BIT의 uint32_t형태로
+// 출력될 것이다!)
+// 변수인자 int*a있다면?->변수의 시작주소(변수의 위치)를 넣어달라는거야!
+
+// 함수 포인터-> 그 함수의 이름위치
+// 즉, 변수에서의 위치가 &a 였다면 함수 void a(void)의 위치는 a인 것이다!!
+// 그럼 콜백함수의 인자 *cbf는 몰까? => 함수의 이름(함수의 위치)을 넣어달라는거야!
+
+// volatile을 쓴 이유?-> 캐시에 올라가서 안돼!!
+// 캐시는 메모리상이 아닌 캐시에 올려놓고 cashe - cpu // 에서만 데이터 교환이 일어난다. 근데 얘는 폴링방식의 주기가 담긴 값이 바뀌어서는 안되는 구조체이다!
 
 volatile THR_T gThrObjs[]  = {
 	{ .period = 500, 	.cbf = adc_thread			},
@@ -25,6 +41,7 @@ volatile THR_T gThrObjs[]  = {
 	{ .cbf = NULL			}
 };
 
+// 폴링방식 -> 
 void polling_init(void)
 {
 	adc_init();
